f8d08fd9bf0717bccf78f17410574d578222ac21
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-07 18:18:27 -0300

    feat: add typed routes flag [#1]

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next'
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/nextjs-static-template',
+  typedRoutes: true,
 }
 
 export default nextConfig
